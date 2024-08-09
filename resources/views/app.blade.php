659d4948c7ff336717bc7ea30a857e09dfe5fa20
silmo-yokohama <silmo.yokohama@gmail.com>
2024-08-09 16:47:14 +0900

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
+
+        <title>Laravel</title>
+
+        @vite('resources/ts/app.tsx')
+        @inertiaHead
+    </head>
+    <body>
+        @inertia
+    </body>
+</html>
