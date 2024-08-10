5ea216cab8ee3681c99ec83a1d05696b624efc36
silmo-yokohama <silmo.yokohama@gmail.com>
2024-08-10 13:57:29 +0900

    add: Inertiaプログレスバーを追加

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,13 +1,24 @@
-import './bootstrap';
-import '../css/app.css';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/inertia-react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import "./bootstrap";
+import "../css/app.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/inertia-react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { InertiaProgress } from "@inertiajs/progress";
+
+InertiaProgress.init({
+  // 色を設定（例: 青色）
+  color: "#4B5563",
+  // トランジションの長さをミリ秒で設定
+  delay: 250,
+  // プログレスバーを表示する最小時間（ミリ秒）
+  includeCSS: true,
+  showSpinner: false,
+});
 
 createInertiaApp({
   resolve: (name) =>
-    resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob("./Pages/**/*.tsx")),
   setup({ el, App, props }) {
     createRoot(el).render(<App {...props} />);
   },
