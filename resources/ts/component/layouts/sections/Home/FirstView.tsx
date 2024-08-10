e4e6ac4b0341c8c3acba8e8debb86c15535b3438
silmo-yokohama <silmo.yokohama@gmail.com>
2024-08-11 01:45:30 +0900

    update: FirstViewにパララックス適用

--- resources/ts/component/layouts/sections/Home/FirstView.tsx
+++ resources/ts/component/layouts/sections/Home/FirstView.tsx
@@ -1,29 +1,30 @@
 import React from "react";
 import { useTheme } from "../../../../hooks/useTheme";
 import { Themes } from "../../../../values/themes";
+import { ParallaxBanner, ParallaxProvider } from "react-scroll-parallax";
 
 const FirstView: React.FC = () => {
   const { activeTheme } = useTheme();
   const imgPath: string = activeTheme === Themes.light
-    ? "/images/photo/yokohama-day.jpg"
+    ? "/images/photo/yokohama-night.jpg"
     : "/images/photo/yokohama-night.jpg";
 
   return (
-    <div
-      className="hero min-h-screen fixed -z-10"
-      style={{
-        backgroundImage: `url(${imgPath})`,
-      }}
-    >
-      <div className="t hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-lg">
-          <h1 className="t digital text-5xl md:text-7xl mb-5 font-bold">
-            Hello World!
-          </h1>
+    <ParallaxProvider>
+      <ParallaxBanner
+        layers={[{ image: imgPath, speed: -55 }]}
+        className="hero h-screen"
+      >
+        <div className="t hero-overlay bg-opacity-60 z-[1]"></div>
+        <div className="hero-content text-neutral-content text-center z-[2]">
+          <div className="max-w-lg">
+            <h1 className="t digital text-5xl md:text-7xl mb-5 font-bold">
+              Hello World!
+            </h1>
+          </div>
         </div>
-      </div>
-    </div>
+      </ParallaxBanner>
+    </ParallaxProvider>
   );
 };
 
