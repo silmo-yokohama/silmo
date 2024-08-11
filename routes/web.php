71b96d4bf2541f1118e349d70a3e84987d0c8124
silmo-yokohama <silmo.yokohama@gmail.com>
2024-08-11 11:26:03 +0900

    add: WordPressへAPIを投げるサービスを作成

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Api\PostController;
 
 Route::get('/', function () {
-    return Inertia::render('Home');
+  return Inertia::render('Home');
+});
+
+// API routes
+Route::prefix('api')->group(function () {
+  Route::get('/news/latest', [PostController::class, 'getLatestPosts'])->name('api.latestNews');;
 });
